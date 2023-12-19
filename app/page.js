604fccd214e1102b10d04b2f021fd9e1a8bb0e2d
PKDGOOD <sean4787@gmail.com>
2023-12-19 15:22:29 +0900

    feat: 영상리스트 좌우 스크롤

--- app/page.js
+++ app/page.js
@@ -13,10 +13,11 @@ const HomePage = () => {
       setChannels([
         {
           id: 1,
-          title: '채널 1',
-          subscribers: '1M 구독자',
+          title: 'NewJeans',
+          profileImage: 'https://yt3.googleusercontent.com/SRpVM-37M6lqLEQyDFaSA5IrgEyr9eONMrkKkAx3g5z4uTNPN78rAWTLC6W2b5uKb2KptX03pQ=s176-c-k-c0x00ffffff-no-rj',
+          subscribers: '682M 구독자',
           videos: [
-            { id: 1, title: '영상 1', thumbnail: 'https://via.placeholder.com/210x118', url: 'https://www.youtube.com/watch?v=example1', uploadDate: '2023-01-01', views: '1M' },
+            { id: 1, title: 'NewJeans(뉴진스) 7', thumbnail: 'https://via.placeholder.com/210x118', url: 'https://www.youtube.com/embed/GPQI83T-cR4', uploadDate: '2023-12-15', views: '69.1M' },
             { id: 2, title: '영상 2', thumbnail: 'https://via.placeholder.com/210x118', url: 'https://www.youtube.com/watch?v=example2', uploadDate: '2023-01-02', views: '2M' },
             { id: 3, title: '영상 3', thumbnail: 'https://via.placeholder.com/210x118', url: 'https://www.youtube.com/watch?v=example3', uploadDate: '2023-01-03', views: '3M' },
           ],
@@ -24,6 +25,7 @@ const HomePage = () => {
         {
           id: 2,
           title: '채널 2',
+          profileIamge: 'https://via.placeholder.com/50',
           subscribers: '500K 구독자',
           videos: [
             { id: 4, title: '영상 4', thumbnail: 'https://via.placeholder.com/210x118', url: 'https://www.youtube.com/watch?v=example4', uploadDate: '2023-01-04', views: '4M' },
@@ -55,7 +57,7 @@ const HomePage = () => {
           channels.map(channel => (
             <div key={channel.id} className={styles.channel}>
               <div className={styles.channelHeader}>
-                <img src="https://via.placeholder.com/50" alt="프로필" className={styles.profileImage} />
+                <img src={channel.profileImage} alt="프로필" className={styles.profileImage} />
                 <h2 className={styles.channelTitle}>{channel.title}</h2>
                 <p className={styles.subscribers}>{channel.subscribers}</p>
                 <button onClick={() => goToChannelDetails(channel.id)} className={styles.detailsButton}>
@@ -66,7 +68,7 @@ const HomePage = () => {
                 {channel.videos.map(video => (
                   <div key={video.id} className={styles.video}>
                     <a href={video.url} target="_blank" rel="noopener noreferrer">
-                      <img src={video.thumbnail} alt={video.title} className={styles.videoThumbnail} />
+                    <iframe width="424" height="238" src={video.url} title={video.title} frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
                       <div className={styles.videoInfo}>
                         <h3 className={styles.videoTitle}>{video.title}</h3>
                         <p className={styles.videoDetails}>{video.uploadDate} · {video.views} 조회수</p>
